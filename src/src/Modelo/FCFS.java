169897b0c8d5b8ee6727598f4c4afea3ab647495
anfebeba2001 <57334505+anfebeba2001@users.noreply.github.com>
2024-04-28 14:42:33 -0500

    Initial commit

---
+++ src/src/Modelo/FCFS.java
@@ -0,0 +1,62 @@
+package src.Modelo;
+
+public class FCFS {
+
+	private Queue<Process> queue;
+	private int currentFinalTime = 0;
+	private int serialId;
+
+	public FCFS() {
+		queue = new Process>();
+		serialId = 0;
+	}
+
+	private int randomBurst() {
+		return (int) (Math.random() * 10 + 1);
+	}
+
+	private void calcuteTime(Process process) {
+		process.setStartTime(currentFinalTime);
+		process.setFinalTime(process.getBurstTime() + process.getStartTime());
+		process.setReturnTime(process.getFinalTime() - process.getArrivalTime());
+		process.setWaitTime(process.getReturnTime() - process.getBurstTime());
+		currentFinalTime = process.getFinalTime();
+	}
+
+	public Object[] getDataProcess(Process process) {
+		calcuteTime(process);
+		return process.resume();
+	}
+
+	public Object[] addProcess() {
+		Process process = new Process(serialId + 1, serialId, randomBurst());
+		queue.add(process);
+		serialId++;
+		return process.resume();
+	}
+
+	public Object[] appendProcess(Process process) {
+		queue.add(process);
+		return process.resume();
+	}
+
+	public Process pollProcess() {
+		return queue.poll();
+	}
+
+	public int getSerialId() {
+		return serialId;
+	}
+
+	public int getCurrentFinalTime() {
+		return currentFinalTime;
+	}
+
+	public boolean isQueueEmpty() {
+		return queue.isEmpty();
+	}
+
+	public void finalizeQueue() throws Throwable {
+		queue.finalize();
+	}
+}
---
+++ src/src/Vista/GanttPanel.java
@@ -0,0 +1,38 @@
+package src.Vista;
+
+import javax.swing.JFrame;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableModel;
+
+public class GanttPanel extends TablePanel {
+
+	public GanttPanel(JFrame window, String title) {
+		super(window, title);
+		getTable().setShowGrid(false);
+		getTable().setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+		setTableModel(new DefaultTableModel(new Object[]{"PID"}, 0));
+		TableCellRedendererColor cellRenderer = new TableCellRedendererColor();
+		cellRenderer.setHorizontalAlignment(SwingConstants.CENTER);
+		getTable().setDefaultRenderer(Object.class , cellRenderer);
+	}
+
+	public void paintProcess(Object[] process) {
+		int row = getTableModel().getRowCount();
+		getTableModel().addRow(new Object[]{process[0]});
+		paintBurstTime(row, (int) process[3], (int) process[4]);
+		paintWaitTime(row, (int) process[1], (int) process[3]);
+	}
+	
+	private void paintWaitTime(int row, int arrivalTime, int startTime) {
+		for (int i = arrivalTime; i < startTime; i++)
+			getTableModel().setValueAt("  ", row, i + 1);
+	}
+	
+	private void paintBurstTime(int row, int startTime, int finalTime){
+		for (int i = startTime; i < finalTime; i++) {
+			getTableModel().addColumn(String.valueOf(i));
+			getTableModel().setValueAt(" ", row, i + 1);
+		}
+	}	
+}
